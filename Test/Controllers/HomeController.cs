246576c54e661c5954b36da66d1c09a027217958
Xhxstart <1026895465@qq.com>
2017-11-15 11:27:41 +0900

    first commit

---
+++ Test/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Test.Models;
+
+namespace Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Home/Index2
+        public ActionResult Index2()
+        {
+            return View();
+        }
+
+        // Post: /Home/Login
+        [HttpPost, Route("/Login")]
+        public async Task<JsonResult> Login(User user)
+        {
+            return await Task.Run(() =>
+             {
+                 string username = Request["username"];
+                 string pwd = Request["pwd"];
+
+                 message msg = null;
+
+                 if (username == "rain" && pwd == "m123")
+                 {
+                     msg = new message(true, "Success");
+                 }
+                 else
+                 {
+                     msg = new message(false, "Fail");
+                 }
+
+                 return Json(msg);
+             });
+        }
+
+        // put: /Home/Login
+        //[HttpPut, Route("/Login")]
+        //public JsonResult Login1()
+        //{
+        //    string username = Request["username"];
+        //    string pwd = Request["pwd"];
+
+        //    message msg = null;
+
+        //    if (username == "rain" && pwd == "m123")
+        //    {
+        //        msg = new message(true, "Success");
+        //    }
+        //    else
+        //    {
+        //        msg = new message(false, "Fail");
+        //    }
+
+        //    return Json(msg);
+        //});
+        //}
+    }
+}
---
+++ Test/Models/message.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Test.Models
+{
+    public class message
+    {
+        private bool success;
+        private string msg;
+
+        public message(bool success, string msg)
+        {
+            this.success = success;
+            this.msg = msg;
+        }
+
+        public bool Success
+        {
+            get { return success; }
+            set { success = value; }
+        }
+
+        public string Msg
+        {
+            get { return msg; }
+            set { msg = value; }
+        }
+    }
+}
